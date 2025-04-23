ef3cdadbddf27b2cda8e38f4e5960987c3a93639
Caio Silva <164821724+CaioS1lv2@users.noreply.github.com>
2025-04-23 20:45:52 -0300

    Create Ex04.py

---
+++ Ex04.py
@@ -0,0 +1,8 @@
+# medindo dimensao da parede e de quanta tinta seria necessario para pintar a parede
+largura = float(input('Largura da parede: '))
+altura = float(input('Altura da parede: '))
+dimen = pare1 * pare2
+   print(f'Sua parede tem a dimensao de {largura}x{altura} e sua área é de {dimen}m)
+tinta = dimen / 2
+   print(f'Para pintar essa area vc ira precisar de {tinta}l de tinta')
+
